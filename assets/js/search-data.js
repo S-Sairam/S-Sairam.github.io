f27cd19c37df3889d8fcd6f6cd861eb974de174d
S-Sairam <125436937+S-Sairam@users.noreply.github.com>
2025-10-25 07:32:25 +0000

    Deploying to gh-pages from @ S-Sairam/S-Sairam.github.io@471bdd31cf7b03a7d3e527138a5f1ea1edecbcdb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,6 +88,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
@@ -103,6 +113,11 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
             },},{id: "projects-bcmem",
           title: 'BCMEM',
           description: "Architectural Regularization via Number-Theoretic Priors",
