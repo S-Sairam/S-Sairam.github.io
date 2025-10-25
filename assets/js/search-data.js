c0581690829e85fa70165bbe7cee2aa3cd3b41a5
S-Sairam <125436937+S-Sairam@users.noreply.github.com>
2025-10-25 06:55:48 +0000

    Deploying to gh-pages from @ S-Sairam/S-Sairam.github.io@1e909a59e1b0941c7571b8545c235bf6f0f31f4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,12 +121,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -166,6 +161,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-bcmem",
+          title: 'BCMEM',
+          description: "Architectural Regularization via Number-Theoretic Priors",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/BCMEM/";
             },},{
         id: 'social-email',
         title: 'email',
