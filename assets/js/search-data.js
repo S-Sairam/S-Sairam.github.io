64466b983abea47fc4b3e2db76884483f9a17d10
S-Sairam <125436937+S-Sairam@users.noreply.github.com>
2025-11-03 01:07:42 +0000

    Deploying to gh-pages from @ S-Sairam/S-Sairam.github.io@46bb153c1bfac7d56ca9ba6ce3e073284a7e995f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,20 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-why-reproducibility-is-the-ultimate-teacher-a-clean-room-sam-replication",
         
           title: "Why Reproducibility is the Ultimate Teacher: A Clean-Room SAM Replication",
